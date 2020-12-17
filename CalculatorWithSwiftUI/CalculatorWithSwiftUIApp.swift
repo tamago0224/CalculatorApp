23f339f72df84760df81d81098abe1e8e017a006
tamago0224 <bigtreesasuke200733@gmail.com>
2020-12-17 23:25:16 +0900

    Initial Commit

---
+++ CalculatorWithSwiftUI/CalculatorWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorWithSwiftUIApp.swift
+//  CalculatorWithSwiftUI
+//
+//  Created by たまご on 2020/12/17.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
